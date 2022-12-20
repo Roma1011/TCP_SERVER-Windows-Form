26fe7e28f0ef51d8113e4d3e72937a9fc83571d1
R.Kekua <rkekua@seu.edu.ge>
2022-12-20 22:30:59 +0400

    Add project files.

---
+++ TCP_SERVER/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Data;
+using System.Text;
+using System.ComponentModel;
+using System.Drawing;
+using System.Net;
+namespace TCP_SERVER
+{
+	public partial class Form1 : Form
+	{
+		TcpListener mTcpListener;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
